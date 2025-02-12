431a9aea846ccd8f0b2a006e88d27c446f5ce615
iamDyeus <dyeusyt@gmail.com>
2025-02-12 15:52:58 +0530

    Add HeroGeometric component and update Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
 import { ThemeToggle } from "@/components/theme-toggle";
+import HeroGeometric from "@/components/ui/hero-geometric";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        
-        <ThemeToggle/>
-      </main>
-      
-    </div>
+    <>
+      <HeroGeometric badge="iamDyeus/MbGa" title1="Make Baddies'" title2="Great Again" />
+    </>
   );
 }
